f4f312df6462a41971f8685027c53a955146506a
Shawn Zhao <shawnzhao0518@gmail.com>
2026-02-27 15:51:43 +1100

    Delete src directory

--- src/components/MetricCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function MetricCard({ label, value, hint }) {
-  return (
-    <div className="card metric-card">
-      <p className="metric-label">{label}</p>
-      <h3 className="metric-value">{value}</h3>
-      {hint ? <p className="metric-hint">{hint}</p> : null}
-    </div>
-  );
-}
